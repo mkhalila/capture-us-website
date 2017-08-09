06157419729ade45a6825468cf983b76b4b609db
Khalil Ahmed <khalil@Mohamads-MacBook-Pro.local>
2017-08-09 21:26:34 +0100

    Remove placeholder quotes and add first real quote

--- js/scripts.js
+++ js/scripts.js
@@ -65,18 +65,7 @@ function initCarousel() {
 function quoteAutomation() {
 	// Quote automation
   var quotes = [
-    "Quote 1",
-    "Quote 2",
-    "Quote 3",
-    "Quote 4",
-    "Quote 5",
-    "Quote 6",
-    "Quote 7",
-    "Quote 8",
-    "Quote 9",
-    "Quote 10",
-    "Quote 11",
-    "Quote 12"
+    "Amazing setup, u guys delivered. Honestly love you guys"
   ];
 
   randNumber = Math.random() * (quotes.length - 1);
